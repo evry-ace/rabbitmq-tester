b639fe8baa17c83e272f3e1eb6fc2689f266eab6
Stian Haugland <stian.haugland@evry.com>
2021-04-19 10:48:44 +0200

    Add rabbitMQ producer and consumer

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/evry-ace/rabbitmq-tester
 
 go 1.16
+
+require github.com/streadway/amqp v1.0.0 // indirect
--- main.go
+++ main.go
@@ -1,4 +1,87 @@
 package main
 
+import (
+	"log"
+	"github.com/streadway/amqp"
+)
+
+
 func main(){
+		//consumer()
+		producer()
 }
+
+func failOnError(err error, errMsg string){
+	if err != nil {
+		log.Fatalf("%s:,%s",errMsg,err)
+	}
+}
+
+func setupConnection() (*amqp.Connection, *amqp.Channel, amqp.Queue ) {
+	log.Println("Connecting to RabbitMQ")
+	conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
+	failOnError(err,"Failed to connect to RabbitMQ")
+	
+	log.Println("Opening a channel to RabbitMQ")
+	ch, err := conn.Channel()
+	failOnError(err,"Failed to open a channel")
+	
+	log.Println("Declaring a que")
+	que, err := ch.QueueDeclare(
+		"test_monitor_que",		//name
+		false,								//durable
+		false,								//delete when unused
+		false,								//exclusive
+		false,								//no-wait
+		nil,									//arguments
+	)
+	failOnError(err, "Failed to declare a que")
+	return conn,ch,que
+}
+
+func producer() {
+	conn,ch,que := setupConnection()
+	defer conn.Close()
+	defer ch.Close()
+
+	log.Println("Publishing a message")
+	body := "Hello World!"
+	err := ch.Publish(
+		"",     		// exchange
+		que.Name, 	// routing key
+		false,  		// mandatory
+		false,  		// immediate
+		amqp.Publishing {
+			ContentType: "text/plain",
+			Body:        []byte(body),
+		})
+		failOnError(err, "Failed to publish a message")
+		log.Println("Message published")			
+	}
+	
+	func consumer(){
+		conn,ch,que := setupConnection()
+		defer conn.Close()
+		defer ch.Close()
+
+		msgs, err := ch.Consume(
+			que.Name, // queue
+			"",     // consumer
+			true,   // auto-ack
+			false,  // exclusive
+			false,  // no-local
+			false,  // no-wait
+			nil,    // args
+		)
+		failOnError(err, "Failed to register a consumer")
+		
+		forever := make(chan bool)
+
+		go func() {
+			for d := range msgs {
+				log.Printf("Recived a message: %s", d.Body)
+			}
+		}()
+		log.Printf(" [*] Waiting for messages. To exit press CTRL+C")
+		<-forever
+	}
