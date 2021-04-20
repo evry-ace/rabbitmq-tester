38afe8068c5eef4d548130e006d71a84df146fa2
Stian Haugland <stian.haugland@evry.com>
2021-04-20 18:11:48 +0200

    Add rabbitMQ producer and consumer that use fanout exchange

--- main.go
+++ main.go
@@ -1,87 +1,136 @@
 package main
 
 import (
+	"flag"
 	"log"
+	"time"
+
 	"github.com/streadway/amqp"
 )
 
+var rmqAddr = flag.String("rabbitmq-address","amqp://guest:guest@localhost:5672/","The RabbitMQ address")
+var exchangeName = flag.String("exchange-name","rbmq-test","The name of the exchange to publish to / consume messages from")
+var isProducer = flag.Bool("is-producer", true, "Default true, if false type == consumer")
+var testInterval = flag.Int("test-interval-seconds", 5, "Interval between messages beeing produced/consumed")
 
-func main(){
-		//consumer()
+func main() {
+	flag.Parse()
+
+	if *isProducer {
 		producer()
-}
-
-func failOnError(err error, errMsg string){
-	if err != nil {
-		log.Fatalf("%s:,%s",errMsg,err)
+	} else {
+		consumer()
 	}
 }
 
-func setupConnection() (*amqp.Connection, *amqp.Channel, amqp.Queue ) {
-	log.Println("Connecting to RabbitMQ")
-	conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
-	failOnError(err,"Failed to connect to RabbitMQ")
-	
-	log.Println("Opening a channel to RabbitMQ")
+func setupConnection() (*amqp.Connection, *amqp.Channel, error) {
+	conn, err := amqp.Dial(*rmqAddr)
+	if err != nil {
+		log.Printf("Failed to connect to RabbitMQ: %s", err)
+	}
 	ch, err := conn.Channel()
-	failOnError(err,"Failed to open a channel")
-	
-	log.Println("Declaring a que")
-	que, err := ch.QueueDeclare(
-		"test_monitor_que",		//name
-		false,								//durable
-		false,								//delete when unused
-		false,								//exclusive
-		false,								//no-wait
-		nil,									//arguments
+	if err != nil {
+		log.Printf("Failed to open a channel: %s", err)
+	}
+
+	err = ch.ExchangeDeclare(
+		*exchangeName,
+		"fanout",
+		true,
+		false,
+		false,
+		false,
+		nil,
 	)
-	failOnError(err, "Failed to declare a que")
-	return conn,ch,que
+	if err != nil {
+		log.Printf("Failed to declare an exchange: %s", err)
+	}
+	return conn, ch, err
 }
 
 func producer() {
-	conn,ch,que := setupConnection()
-	defer conn.Close()
-	defer ch.Close()
-
-	log.Println("Publishing a message")
-	body := "Hello World!"
-	err := ch.Publish(
-		"",     		// exchange
-		que.Name, 	// routing key
-		false,  		// mandatory
-		false,  		// immediate
-		amqp.Publishing {
-			ContentType: "text/plain",
-			Body:        []byte(body),
-		})
-		failOnError(err, "Failed to publish a message")
-		log.Println("Message published")			
-	}
-	
-	func consumer(){
-		conn,ch,que := setupConnection()
+	conn, ch, err := setupConnection()
+	if err != nil {
+		log.Println(err)
+	}else{
 		defer conn.Close()
 		defer ch.Close()
-
-		msgs, err := ch.Consume(
-			que.Name, // queue
-			"",     // consumer
-			true,   // auto-ack
-			false,  // exclusive
-			false,  // no-local
-			false,  // no-wait
-			nil,    // args
-		)
-		failOnError(err, "Failed to register a consumer")
-		
-		forever := make(chan bool)
-
-		go func() {
-			for d := range msgs {
-				log.Printf("Recived a message: %s", d.Body)
-			}
-		}()
-		log.Printf(" [*] Waiting for messages. To exit press CTRL+C")
-		<-forever
 	}
+	
+	ticker := time.NewTicker(time.Duration(float64(*testInterval)) * time.Second)
+	quit := make(chan struct{})
+	forever := make(chan bool)
+
+	go func() {
+		for {
+			select {
+			case <-ticker.C:
+				err = ch.Publish(
+					*exchangeName,
+					"",
+					false,
+					false,
+					amqp.Publishing{
+						ContentType: "text/plain",
+						Body:        []byte("hello world!"),
+					})
+			case <-quit:
+				conn.Close()
+				ch.Close()
+				ticker.Stop()
+				return
+			}
+		}
+	}()
+	<-forever
+}
+
+func consumer() {
+	conn, ch, err := setupConnection()
+	if err != nil {
+		log.Printf("setupConnection failed: %s",err)
+	}else {
+		defer conn.Close()
+		defer ch.Close()
+	}
+	q, err := ch.QueueDeclare(
+		"",
+		false,
+		false,
+		true,
+		false,
+		nil,
+	)
+	if err != nil {
+		log.Printf("Failed to declare queue: %s", err)
+	}
+
+	err = ch.QueueBind(
+		q.Name,
+		"",
+		*exchangeName,
+		false,
+		nil,
+	)
+
+	if err != nil {
+		log.Printf("Failed to bind queue to exchange: %s", err)
+	}
+
+	msgs, err := ch.Consume(
+		q.Name,
+		"",
+		false,
+		false,
+		false,
+		false,
+		nil,
+	)
+	if err != nil {
+		log.Printf("Failed to register a consumer: %s", err)
+	}
+	for d := range msgs {
+		log.Printf("Message recived: %s",d.Body)
+		d.Ack(false)
+	}
+}
